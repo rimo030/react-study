aa0c20448f728ba25216dfe689056de9dae05bf8
rimo <muank.dev@gamil.com>
2024-08-26 22:34:01 +0900

    feat: react init

--- src/App.js
+++ src/App.js
@@ -1,56 +1,25 @@
-import React from "react";
-import Header from "./components/Header.js";
-import SearchForm from "./components/SearchForm.js";
-import SearchResult from "./components/SearchResult.js";
-import store from './Store.js'
+import logo from './logo.svg';
+import './App.css';
 
-export default class App extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            searchKeyword: ""
-            , SearchResult: []
-            , submitted: false
-        }
-    }
-
-    handleChangeInput(searchKeyword) {
-        if (searchKeyword.length <= 0) {
-            this.handleReset();
-        }
-        this.setState({ searchKeyword })
-    }
-
-    search(searchKeyword) {
-        const searchResult = store.search(searchKeyword);
-
-        this.setState({
-            searchResult,
-            submitted: true,
-        })
-    }
-
-    handleReset() {
-        this.setState({ searchKeyword: "", searchResult: [], submitted: false })
-    }
-
-    render() {
-        const { searchKeyword, submitted, searchResult } = this.state;
-        return (
-            <>
-                <Header title="검색" />
-                <div className={"container"}>
-                    <SearchForm
-                        value={searchKeyword}
-                        onChange={(value) => this.handleChangeInput(value)}
-                        onSubmit={() => this.search(searchKeyword)}
-                        onReset={() => this.handleReset()} />
-
-                    <div className="content">
-                        {submitted && <SearchResult data={searchResult} />}
-                    </div>
-                </div>
-            </>
-        )
-    }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
+
+export default App;
--- src/main.js
+++
@@ -1,7 +0,0 @@
-import ReactDOM from 'react-dom'
-import React from "react";
-import App from "./App.js";
-
-console.log("main.js");
-
-ReactDOM.render(<App />, document.querySelector("#app"));
